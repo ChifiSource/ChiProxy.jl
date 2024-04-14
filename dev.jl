dbb6eaa577f987d150ff54382c12089c8974db3c
emmy <52672675+emmaccode@users.noreply.github.com>
2024-04-14 17:49:12 -0600

    added new multiroute load balancing

--- dev.jl
+++ dev.jl
@@ -1,8 +1,10 @@
 using Pkg; Pkg.activate(".")
 using Toolips
 using ChiProxy
-using ChiProxy: TestServer
+using ChiProxy: TestServer1, TestServer2
 
-Toolips.start!(ChiProxy, "127.0.0.1":80)
+Toolips.start!(ChiProxy, "192.168.1.15":80)
 
-Toolips.start!(TestServer)
+Toolips.start!(TestServer1, "127.0.0.1":8000)
+
+Toolips.start!(TestServer2, "127.0.0.1":8001)
--- src/ChiProxy.jl
+++ src/ChiProxy.jl
@@ -5,14 +5,21 @@ import Toolips: route!, AbstractConnection, getindex, Route
 
 abstract type AbstractProxyRoute <: Toolips.AbstractRoute end
 
-abstract type AbstractSource end
+abstract type ProxyMultiRoute <: AbstractProxyRoute end
 
-abstract type Balanced{T <: Number} end
+mutable struct BalancedMultiRoute <: ProxyMultiRoute
+    path::String
+    sources::Vector{AbstractProxyRoute}
+    loads::Vector{Float64}
+    status::Pair{Int64, Int64}
+    scale::Int64
+end
+
+abstract type AbstractSource end
 
 struct Source{T <: Any} <: AbstractSource
     sourceinfo::Dict{Symbol, <:Any}
 end
-
 
 getindex(src::Source{<:Any}, info::Symbol) = getindex(src.sourceinfo, info)
 
@@ -30,7 +37,7 @@ mutable struct SourceRoute{T <: AbstractConnection, SOURCE <: Any} <: AbstractSo
 end
 
 function route!(c::Toolips.AbstractConnection, pr::AbstractProxyRoute)
-    Toolips.proxy_pass!(c, "http://$(string(pr.ip4))")
+    Toolips.proxy_pass!(c, "http://$(string(pr.ip))")
 end
 
 route!(c::Connection, vec::Vector{<:AbstractProxyRoute}) = begin
@@ -46,12 +53,40 @@ route!(c::Connection, vec::Vector{<:AbstractProxyRoute}) = begin
     end
 end
 
+function route!(c::Toolips.AbstractConnection, pr::AbstractSourceRoute)
+    rt = source!(c, pr.src)
+end
+
+function route!(c::Toolips.AbstractConnection, pr::BalancedMultiRoute)
+    current_source::Int64 = pr.status[1]
+    current_count::Int64 = pr.status[2]
+    if current_count / pr.scale >= pr.loads[current_source]
+        current_source += 1
+        current_count = 0
+        if current_source > length(pr.sources)
+            current_source = 1
+        end
+    end
+    current_count += 1
+    pr.status = current_source => current_count
+    route!(c, pr.sources[current_source])
+end
+
 proxy_route(hostname::String, send_to::IP4) = begin
     ProxyRoute{Connection}(hostname, send_to)::ProxyRoute{Connection}
 end
 
-function route!(c::Toolips.AbstractConnection, pr::AbstractSourceRoute)
-    rt = source!(c, pr.src)
+proxy_route(path::String, routes::Pair{Float64, <:AbstractProxyRoute} ...; scale::Int64 = 100) = begin
+    loads::Vector{Float64} = Vector{Float64}()
+    new_routes::Vector{AbstractProxyRoute} = Vector{AbstractProxyRoute}()
+    for r in routes
+        push!(loads, r[1])
+        push!(new_routes, r[2])
+    end
+    if sum(loads) != 1.0
+        throw(Toolips.RouteError("balanced proxy-route", "load balances must add up to 100 (percent)."))
+    end
+    BalancedMultiRoute(path, new_routes, loads, 1 => 0, scale)
 end
 
 function source(path::String, to::IP4)
@@ -60,15 +95,8 @@ function source(path::String, to::IP4)
     SourceRoute{Connection, IP4}(path, src)
 end
 
-
 function source!(c::Toolips.AbstractConnection, source::Source{IP4})
     Toolips.proxy_pass!(c, "http://$(string(source[:ip]))")
-end
-
-
-function source(f::Function, path::String, to::IP4)
-    srcinfo::Dict{Symbol, String} = Dict{Symbol, String}(:backup => f, :to => to)
-    
 end
 
 function source(path::String, to::Toolips.AbstractRoute)
@@ -92,78 +120,34 @@ function source!(c::Toolips.AbstractConnection, source::Source{File})
     write!(c, fl)
 end
 
-function source(path::String, loads::SourceRoute{<:Any, <:Any} ...)
-    srcinfo::Dict{Symbol, Any} = Dict{Symbol, Any}(:routes => [root for root in loads], :at => 1)
-    src = Source{Balanced{Integer}}(srcinfo)
-    SourceRoute{Connection, Balanced{Integer}}(path, src)
-end
 
-function source!(c::Toolips.AbstractConnection, source::Source{Balanced{Integer}})
-    i = source[:at]
-    routes = source[:routes]
-    if source[:at] > length(routes)
-        source.sourceinfo[:at] = 1
-    end
-    source!(c, routes[source[:at]].src)
-    source.sourceinfo[:at] += 1
-end
+server1 = proxy_route("192.168.1.15", "127.0.0.1":8000)
+server2 = proxy_route("192.168.1.15", "127.0.0.1":8001)
 
-function source(path::String, loads::Pair{Int64, SourceRoute{<:Any, <:Any}} ...)
-    loadd = Dict{Int64, Int64}(p[1] => e for (e, p) in enumerate(loads))
-    max = maximum(keys(loadd))
-    srcinfo::Dict{Symbol, Any} = Dict{Symbol, Any}(:routes => [rootp[2] for rootp in loads],
-    :loads => loadd, :at => 1, :active => 1)
-    src = Source{Balanced{Int64}}(srcinfo)
-    SourceRoute{Connection, Balanced{Int64}}(path, src)
-end
-
-function source!(c::Toolips.AbstractConnection, source::Source{Balanced{Int64}} ...)
-    i = source[:at]
-    loadd = source[:loads]
-    if i == loadd[source.sourceinfo[:active]]
-        source.sourceinfo[:active] += 1
-        if :active > length(routes)
-            source.sourceinfo[:active] = 1
-        end
-        source.sourceinfo[:at] = 1
-    end
-    source!(c, routes[source.sourceinfo[:active]])
-    i += 1
-end
-
-function source(path::String, scale::Int64, loads::Pair{Float64, SourceRoute{<:Any, <:Any}})
-
-end
-
-#==
-function route(r::ProxyRoute{<:Any, <:Any} ...)
-
-end
-==#
-
-
-module ProxyProcessSample
-    using Toolips
-    r = route("/") do c::Connection
-        write!(c, "we served this route through our proxy server.")
-    end
-end
-server1 = source("127.0.0.1:8000", "127.0.0.1":8002)
-server2 = source("127.0.0.1:8000", ProxyProcessSample.r)
-newtest = source("127.0.0.1:8000", server1, server2)
 # 404
 err_404 = Toolips.default_404
 
-module TestServer
+balances = proxy_route("192.168.1.15", .5 => server1, .5 => server2, scale = 10)
+
+module TestServer1
 using Toolips
 main = route("/") do c::Connection
-    write!(c, "</br>this is 127.0.0.1 responding")
+    write!(c, "message from server 1")
 end
 
 export main
 end
 
-export newtest
+module TestServer2
+using Toolips
+main = route("/") do c::Connection
+    write!(c, "message from server 2")
+end
+
+export main
+end
+
+export balances
 end
 
 
